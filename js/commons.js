966a03b83cb9e64e8465a6a48a1d6e1517073d57
unknown <sndream1371@gmail.com>
2020-12-04 11:10:04 +0900

    .

--- js/commons.js
+++ js/commons.js
@@ -19,10 +19,18 @@ function goUrl(btnParm) {
   let queryParm = null;
   switch(btnParm){
     case 'song_7_sonbu_kim' : queryParm ='36.036767,127.160053'; break;
-  
+    case 'song_6_sonbu_park' : queryParm ='36.033652,127.154397'; break;
+    case 'song_9_sonbu_seo' : queryParm ='36.029207,127.180327'; break;
+    case 'song_6_son_mongchun' : queryParm ='36.028864,127.179256'; break;
+    case 'song_juminju' : queryParm ='36.029762,127.179788'; break;
+    case 'song_eunsanjung_sinpungan' : queryParm ='36.029807,127.179928'; break;
+    case 'song_9_son_sangbuk' : queryParm ='36.049299,127.187954'; break;
+    case 'song_8_son_younghak' : queryParm ='36.049378,127.187958'; break;
+    case 'song_8_sonbu_lee' : queryParm ='36.057006,127.193079'; break;
+    case 'song_9_son_sangwon' : queryParm ='36.055308,127.185688'; break;
+    case 'song_7_son_oukwon' : queryParm ='36.054447,127.221850'; break;
   }
 
   window.location.href = GOURL + queryParm;
-
 }
 
