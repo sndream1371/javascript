4e96f0f2dfd0b55286ffac14a9c5982e36027476
unknown <sndream1371@gmail.com>
2020-12-07 11:43:41 +0900

    clean push

--- js/commons.js
+++ js/commons.js
@@ -1,16 +1,11 @@
 'use strict' //엄격모드
 
-function sum( num1, num2 ){
-  return num1 + num2;
-}
-
-
 //const button = document.querySelector('button'); //첫번째 일된된 selector만 만환
-const selectButtons = document.querySelectorAll('button'); //일치된 모든 selector반환
-console.log(selectButtons)
-selectButtons.forEach(function(userItem) {
-  console.log(userItem)  
-});
+// const selectButtons = document.querySelectorAll('button'); //일치된 모든 selector반환
+// console.log(selectButtons)
+// selectButtons.forEach(function(userItem) {
+//   console.log(userItem)  
+// });
 
 function goUrl(btnParm) {
   console.log(btnParm);
